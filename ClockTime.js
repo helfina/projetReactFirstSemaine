85f8da15b561eca8effcd6c7afd4b230b63d63db
gaelle kerforne <stage.seeweb@gmail.com>
2020-12-29 23:07:51 +0100

    ClockTime.js: rendu de l'horloge analogique

--- ClockTime.js
+++ ClockTime.js
@@ -36,16 +36,7 @@ class ClockTime extends React.Component{
                 </div>
 
                 {
-                    /* Appelez un ou plusieurs composants ici
-                    Que ton horloge ait deux modes possibles
-                    Tu utiliserais une props pour définir à lappel du composant le mode choisi
-                    Et dans ta page tu afficherais deux horloges, une de chaque mode
-                    Ya un mode comme actuellementr
-                    Et un nouveau mode,
-                    Où les aiguilles bougent de manière fluide, et non plus sacadée, seconde par seconde
-                    Est-ce que tu vois ce que je veux dire ?
-                    Comme si le temps s'écoulait comme de l'eau, au lieu d'avoir comme des "marches", qui sont les secondes
-                    */
+
                     <p>
                         {this.state.dateTime.toDateString()}<br/>
                         {this.state.dateTime.toLocaleTimeString()}
@@ -59,26 +50,30 @@ class ClockTime extends React.Component{
         const hoursHand = document.querySelector('.hours');
         const minutesHand = document.querySelector('.minutes');
         const secondsHand = document.querySelector('.seconds');
+
         let now = new Date;
-        let seconds = now.getSeconds();
+
         let miliSeconds = now.getMilliseconds();
-        let minutes = now.getMinutes();
-        let hours = now.getHours();
+        let seconds = now.getSeconds() + miliSeconds / 1000;
+        let minutes = now.getMinutes() + seconds / 60 ;
+        let hours = now.getHours() + minutes / 60 ;
+
         let hoursAngle = (360 * hours) / 12;
         let minutesAngle = (360 * minutes) / 60;
-        let secondsAngle = (360 * seconds) / 60 * (miliSeconds / 100);
+
+        //1 Millisecondes = 0.001 Secondes
+        let secondsAngle = (360 * seconds) / 60;
 
         hoursHand.style.transform = `rotate(${hoursAngle}deg) translate(0, -50%)`;
         minutesHand.style.transform = `rotate(${minutesAngle}deg) translate(0, -50%)`;
         secondsHand.style.transform = `rotate(${secondsAngle}deg) translate(0, -50%)`;
 
-        console.log("coucou");
 
         this.setState({
             dateTime: now,
             seconds : seconds,
-            minutes : minutes,
-            hours :hours,
+            minute : minutes,
+            hours :  hours,
         })
 
     }
